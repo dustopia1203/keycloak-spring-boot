424b5e176ac222763aa272cfba3662cfcf06a01a
HieuNguyen <128913585+dustopia1203@users.noreply.github.com>
2024-07-04 21:12:08 +0700

    init(prj): init prj

---
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import com.example.demo.security.KeycloakJwtAuthenticationConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final KeycloakJwtAuthenticationConverter converter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> registry.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(converter)))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello Keycloak!";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('client_admin')")
+    public String adminHello() {
+        return "Hello Keycloak - ADMIN!";
+    }
+
+}
